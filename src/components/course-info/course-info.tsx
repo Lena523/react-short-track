cb203319de8e2c582e157678b638f33b7fa35abf
Elena Khadasevich <camatsu90@mail.ru>
2026-02-01 16:02:46 +0300

    feat: add BackToCourses function

--- src/components/course-info/course-info.tsx
+++ src/components/course-info/course-info.tsx
@@ -3,26 +3,40 @@ import Title from './course-info-title/course-info-title';
 import BackButton from './back-button/back-button';
 import { CourseInfoCardProps } from '@/components/lib/types';
 import CourseInfoCard from './course-info-card';
+import { useState } from 'react';
+import Courses from '../courses/courses';
 
 export default function CourseInfo({ course }: CourseInfoCardProps) {
+  const [backToCourses, setBackToCourses] = useState(false);
+
+  const handleBackToCourses = () => {
+    setBackToCourses(true);
+  };
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '30px',
-        margin: '0 auto',
-        maxWidth: '900px',
-        paddingTop: '50px',
-      }}
-    >
-      <Title text={course.title} />
-      <CourseInfoCard course={course} />
-      <BackButton
-        action={'BACK'}
-        onClick={() => console.log('')}
-        isDisabled={false}
-      />
-    </Box>
+    <>
+      {backToCourses ? (
+        <Courses />
+      ) : (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '30px',
+            margin: '0 auto',
+            maxWidth: '900px',
+            paddingTop: '50px',
+          }}
+        >
+          <Title text={course.title} />
+          <CourseInfoCard course={course} />
+          <BackButton
+            action={'BACK'}
+            onClick={handleBackToCourses}
+            isDisabled={false}
+          />
+        </Box>
+      )}
+    </>
   );
 }
