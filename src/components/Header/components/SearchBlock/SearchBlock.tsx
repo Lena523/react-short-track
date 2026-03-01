45445e45141b9e405a14a00b9af09419f239a136
Elena Khadasevich <camatsu90@mail.ru>
2026-03-01 15:49:52 +0300

    feat: add search functionality

--- src/components/Header/components/SearchBlock/SearchBlock.tsx
+++ src/components/Header/components/SearchBlock/SearchBlock.tsx
@@ -3,13 +3,25 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import useCheckLocation from '@/hooks/useCheckLocation';
+import { textFieldSearchSx } from '@components/types/movies-types';
+import { useSearchParams } from 'react-router';
+import { useState } from 'react';
 
 export default function SearcBlock() {
   const isHomePage = useCheckLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [inputValue, setInputValue] = useState(searchParams.get('search') || '');
 
   if (!isHomePage) {
     return null;
   }
+
+  const handleSearch = () => {
+    setSearchParams({
+      ...Object.fromEntries(searchParams),
+      search: inputValue,
+    });
+  };
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '40px', paddingLeft: '60px' }}>
@@ -18,39 +30,14 @@ export default function SearcBlock() {
         <TextField
           placeholder="What do you want to watch?"
           variant="outlined"
-          sx={{
-            '& .MuiOutlinedInput-root': {
-              background:
-                'linear-gradient(135deg, rgba(70, 70, 70, 0.95) 0%, rgba(40, 40, 40, 0.98) 100%)',
-              borderRadius: '4px',
-              backdropFilter: 'blur(2px)',
-              '& fieldset': {
-                borderColor: 'transparent',
-              },
-              '&:hover fieldset': {
-                borderColor: '#F65261',
-              },
-              '&.Mui-focused fieldset': {
-                borderColor: '#F65261',
-              },
-            },
-            '& .MuiInputBase-input': {
-              padding: '16px 22px',
-              fontSize: '1.2rem',
-              fontWeight: 400,
-              letterSpacing: '0.3px',
-              color: '#ffffff',
-              '&::placeholder': {
-                color: 'rgba(200, 200, 200, 0.7)',
-                opacity: 1,
-                fontWeight: 300,
-                fontSize: '1.1rem',
-              },
-            },
-          }}
+          sx={textFieldSearchSx}
           fullWidth
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
-        <Button variant="redButton">SEARCH</Button>
+        <Button onClick={handleSearch} variant="redButton">
+          SEARCH
+        </Button>
       </Box>
     </Box>
   );
--- src/components/MoviesList/MovieList.tsx
+++ src/components/MoviesList/MovieList.tsx
@@ -2,23 +2,14 @@ import MovieTile from '@components/MoviesList/components/MovieTile/MovieTile';
 import Grid from '@mui/material/Grid';
 import { useGetMoviesQuery } from '@/services/api/apiSlice';
 import Spinner from '@components/common/Spinner/Spinner';
-import type { Movie } from '@/store/store-types';
-import { useMemo } from 'react';
+import { useSearchParams } from 'react-router';
+import { useAppSelector } from '@/store/hooks';
+import { selectFilteredMovies } from '@/store/selectors/movieSelectors';
 
 export default function MovieList() {
-  const { data, isLoading } = useGetMoviesQuery(null);
-
-  const movieTiles: Omit<Movie, 'overview'>[] = useMemo(
-    () =>
-      data?.data.map((movie) => ({
-        id: movie.id,
-        title: movie.title,
-        genres: movie.genres,
-        poster_path: movie.poster_path,
-        release_date: movie.release_date,
-      })) ?? [],
-    [data],
-  );
+  const { isLoading } = useGetMoviesQuery(null);
+  const [searchParams] = useSearchParams();
+  const filteredMovies = useAppSelector((state) => selectFilteredMovies(state, searchParams));
 
   if (isLoading) {
     return <Spinner />;
@@ -26,7 +17,7 @@ export default function MovieList() {
 
   return (
     <Grid container spacing={3}>
-      {movieTiles.map((movie) => (
+      {filteredMovies.map((movie) => (
         <Grid size={{ xs: 12, sm: 6, md: 4 }} key={movie.id}>
           <MovieTile movie={movie} />
         </Grid>
